f166a80a07042df0a51702175ec9e2cf508d6c7c
S-ashton <sophieannashton@icloud.com>
2025-10-20 21:04:19 +0100

    refactor(discount): extract logic for setting discount to Money.zero() when there is no discount

--- cafe-pos-week2/src/main/java/com/cafepos/pricing/NoDiscount.java
+++ cafe-pos-week2/src/main/java/com/cafepos/pricing/NoDiscount.java
@@ -1,4 +1,7 @@
 package com.cafepos.pricing;
+import com.cafepos.common.Money;
 
-public class NoDiscount {
-}
+public final class NoDiscount implements DiscountPolicy {
+    @Override public Money discountOf(Money subtotal) { 
+        return Money.zero(); }
+    }
